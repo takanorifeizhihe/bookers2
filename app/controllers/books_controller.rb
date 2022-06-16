f66394d6e387625daeb8ec0990593b7f7129e141
EC2 Default User <ec2-user@ip-172-31-84-145.ec2.internal>
2022-06-16 17:29:23 +0000

    [finish]create book edit page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)  
   end
   
   def edit
